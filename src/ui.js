7d851653f07609695460e0f2e943c4f6bb63cee1
Cole <propertyisrobbery@hotmail.com>
2022-11-05 21:00:43 -0400

    fix currentweather display add sunrise &sunset

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,9 @@
 import getWeather from './weather';
+import { timeConvert } from './utility';
 
 // query selectors
 const currentCity = document.querySelector('.current-city');
+const country = document.querySelector('.country');
 const currentTemp = document.querySelector('.current-temp');
 const feelsLike = document.querySelector('.feels-like');
 const tempMax = document.querySelector('.temp-max');
@@ -14,14 +16,23 @@ const sunset = document.querySelector('.sunset');
 async function updateView() {
   const data = await getWeather();
 
+  const { convertedSunrise, convertedSunset } = timeConvert(
+    data.sys.sunrise,
+    data.sys.sunset
+  );
+
   currentCity.textContent = data.name;
   currentTemp.textContent = data.main.temp;
-  feelsLike.textContent = data.feels_like;
-  tempMax.textContent = data.temp_max;
-  tempMin.textContent = data.temp_min;
-  humidity.textContent = data.humidity;
-  // sunrise.textContent = data.
-  // sunset.textContent = data.
+  feelsLike.textContent = data.main.feels_like;
+  tempMax.textContent = data.main.temp_max;
+  tempMin.textContent = data.main.temp_min;
+  humidity.textContent = data.main.humidity;
+  country.textContent = data.sys.country;
+  sunrise.textContent = convertedSunrise;
+  sunset.textContent = convertedSunset;
 }
+
+// grab and insert icon on page:
+// let icon = "https://openweathermap.org/img/wn/${iconCode}@2x.png"
 
 export default updateView;
